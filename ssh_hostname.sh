59ff674ad62790ea94b6171d3a274b64a72ceadf
Oscar Carlberg <oscar.carlberg@fortnox.se>
2017-05-08 13:14:57 +0200

    ensure we are running in screen before trying to set stuff

--- ssh_hostname.sh
+++ ssh_hostname.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-hostname_long="${1}"
+if [[ $TERMCAP == *"screen.xterm"* ]]
+then
+	hostname_long="${1}"
 
-[[ $hostname_long == *"fnox.se" ]] && hostname_suffix=".fnox.se"
-[[ $hostname_long == *"noxf.se" ]] && hostname_suffix=".noxf.se"
+	[[ $hostname_long == *"fnox.se" ]] && hostname_suffix=".fnox.se"
+	[[ $hostname_long == *"noxf.se" ]] && hostname_suffix=".noxf.se"
 
-hostname_short=${hostname_long%$hostname_suffix}
+	hostname_short=${hostname_long%$hostname_suffix}
 
-echo -ne "\033k$hostname_short\033\\"
+	echo -ne "\033k$hostname_short\033\\"
+fi
